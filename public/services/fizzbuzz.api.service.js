f7a1c08850e3e9588635b9a625b52c56518c786e
Miguel Lopez <malopezp.91@gmail.com>
2018-03-10 20:01:41 -0800

    Fix local host problem

--- public/services/fizzbuzz.api.service.js
+++ public/services/fizzbuzz.api.service.js
@@ -5,7 +5,7 @@ app.service('fizzBuzzApiService', ['$http', '$q', function($http, $q){
         }
         console.log(data);
 
-        return $http.post("http://localhost:4567/fizzbuzz/long", data).then(function(response){
+        return $http.post("https://boiling-sands-95787.herokuapp.com/fizzbuzz/long", data).then(function(response){
             console.log(response.data);
             return response.data;
         })
